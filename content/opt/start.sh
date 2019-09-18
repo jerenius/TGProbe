65d2a558584d74920d986fda7c654a3e6843c699
Jere Virta <jerenius@gmail.com>
2019-09-18 19:20:52 +0300

    foo

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
-
 clear
 echo "Computer says no."
 echo
 echo "Enumerating I2C bus:"
-
 sudo i2cdetect -y 1
-
-lighty-enable-mod fastcgi
-lighty-enable-mod fastcgi-php
-
-service lighttpd force-reload
-
-
-
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf -n &
-
 trap "supervisorctl shutdown && wait" SIGTERM
-
 wait
